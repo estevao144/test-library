2c76c342ca883c46571ca9cab8c14303998d3521
Estevao <estevo144@gmail.com>
2022-09-19 15:41:22 -0300

    questao 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente NotFound.', () => {
+  test('A página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const titulo = screen.getByRole('heading', {
+      name: 'Page requested not found',
+      level: 2,
+    });
+    const image = screen.getByRole('img');
+
+    expect(titulo).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
